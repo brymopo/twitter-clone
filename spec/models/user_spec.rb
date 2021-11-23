e32e9dfce7efa07e75cf8f34416b06a97c061aec
brymopo <bmoreno.dl@gmail.com>
2021-11-23 18:49:22 -0500

    add alphabetical tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,4 +57,55 @@ RSpec.describe User, type: :model do
       expect(user.following).to match_array(followees)
     end
   end
+
+  describe "scopes" do
+    describe "alphabetical" do
+      let(:user) { create(:user) }
+      let(:names) do
+        [
+          "John Doe",
+          "Pedro Perez",
+          "Andrea Gonzalez",
+          "Andres Zapata",
+          "Maria Bravo"
+        ]
+      end
+      let(:in_order) do
+        [
+          "Andrea Gonzalez",
+          "Andres Zapata",
+          "John Doe",
+          "Maria Bravo",
+          "Pedro Perez"
+        ]
+      end
+      let(:users) { names.map { |name| create(:user, full_name: name) } }
+
+      context "followers" do
+        before do
+          users.each do |u|
+            follow = Follow.create(follower_id: u.id, followed_id: user.id )
+            expect(follow).to be_valid
+          end
+        end
+
+        subject { user.followers.alphabetical.pluck(:full_name) }
+
+        it { is_expected.to eq(in_order) }
+      end
+
+      context "following" do
+        before do
+          users.each do |u|
+            follow = Follow.create(follower_id: user.id, followed_id: u.id )
+            expect(follow).to be_valid
+          end
+        end
+
+        subject { user.following.alphabetical.pluck(:full_name) }
+
+        it { is_expected.to eq(in_order) }
+      end
+    end
+  end
 end
