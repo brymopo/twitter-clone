aadfcb8df1635d13af5e12203a0be6e6ca1aff99
brymopo <bmoreno.dl@gmail.com>
2021-11-24 17:17:18 -0500

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+$counter = 0
+
+test_user = User.create(
+  full_name: "John Doe",
+  password: "abc123",
+  email: "user@example.com",
+  username: "john-doe"
+)
+
+puts "Create test user - Done"
+
+def create_user
+  $counter += 1
+  User.create(
+    full_name: Faker::Name.name,
+    password: Faker::Internet.password,
+    email: "#{Faker::Internet.username}#{$counter}@#{Faker::Internet.domain_name}",
+    username: "#{Faker::Internet.username separators: %w(-)}#{$counter}"
+  )
+end
+
+def create_tweet(user)
+  created_at = Faker::Time.between(from: Date.today - 60, to: Date.today)
+  Tweet.create(
+    message: Faker::Lorem.characters(number: 279),
+    user_id: user.id,
+    created_at: created_at.to_s
+  )
+end
+
+users_list = 50.times.map { create_user }
+users_list << test_user
+
+puts "Create users lists - DONE!"
+
+users_indices = (0...users_list.size).to_a
+
+users_list.each_with_index do |user, ind|
+  puts "-" * 20
+  puts "Seeding data user: #{ind}"
+  puts "Creating user tweets"
+
+  number_of_tweets = Random.rand(15..30)
+  number_of_tweets.times.map { create_tweet(user) }
+
+  puts "Adding followers"
+  number_of_followers = Random.rand(15..25)
+  users_indices.sample(number_of_followers).each do |val|
+    next if val == ind
+
+    Follow.create(followed_id: user.id, follower_id: users_list[val].id)
+  end
+  puts "User done!"
+end
