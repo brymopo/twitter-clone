308264408d0d21de9b721e11a456fefc29507f77
brymopo <bmoreno.dl@gmail.com>
2021-11-25 10:18:15 -0500

    refactor create method of follows controller and add views

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -6,16 +6,21 @@ class FollowsController < ApplicationController
   end
 
   def create
-    @followee = User.find_by!(username: follow_params[:username])
-    @follow = Follow.create!(
-                             follower_id: current_user.id,
-                             followed_id: @followee.id
-                           )
-    flash[:notice] = "Following #{@followee.username}"
-    redirect_to user_path(@followee)
-  rescue StandardError => e
-    flash.now[:alert] = "One or more errors occured. Make sure that the username exists"
-    render :new
+    @followee = User.find_by(username: follow_params[:username])
+    @follow = Follow.new(follower_id: current_user.id)
+
+    if @followee.present?
+      @follow.followed_id = @followee.id
+      @follow.save
+    end
+
+    if @follow.valid? && @followee.present?
+      flash[:notice] = "Following #{@followee.username}"
+      redirect_to user_path(@followee)
+    else
+      flash.now[:alert] = "One or more errors occured. See below"
+      render :new
+    end
   end
 
   private
